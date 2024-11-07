b7f7863b994129cd0da7175a898841c42236aac6
Marijan Juresic <mjuresic@gmail.com>
2024-11-07 02:33:04 +0100

    Add files via upload

---
+++ 001 - Arrays - Duplicate Integer.kt
@@ -0,0 +1,15 @@
+
+// LeetCode 217 - Duplicates
+
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val seen = HashSet<Int>()
+        for (num in nums) {
+            if (num in seen) {
+                return true
+            }
+            seen.add(num)
+        }
+        return false
+    }
+}
